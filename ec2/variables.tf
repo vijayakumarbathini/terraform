d709a10073ed2f9721be6a4f5a4b47a7846671b7
Vijaya <mr.vijay101010@gmail.com>
2024-12-20 15:06:55 +0530

    Correct the protocols

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -20,13 +20,13 @@ variable "ingress_rules" {
     "ssh" = {
       from_port   = 22
       to_port     = 22
-      protocol    = "ssh"
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     },
     "http" = {
       from_port   = 80
       to_port     = 80
-      protocol    = "http"
+      protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
   }
